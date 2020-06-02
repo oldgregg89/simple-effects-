298eabfe46a42a526cb7be7fa4825bb4511d4392
Ian Gregg <iangregg188@gmail.com>
2020-06-02 12:02:45 -0700

    Updated the code so the fade in works and the fade out works but still need to show the click me button after its run both options. <blacchoy@gmail.com> Co-authored-by: Ian Gregg <iangregg188@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,21 @@ $(document).ready(function() {
 
 $(document).ready(function() {
   $(".clickable1").click(function() {
-    $(".walrus-showing1").toggle();
+    $(".walrus-showing1").slideToggle();
     $(".walrus-hidden1").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable2").click(function() {
+    $(".walrus-showing2").fadeIn(2000);
+    $(".walrus-hidden2").fadeIn(2000);
+  });
+});
+
+$(document).ready(function() {
+  $(".clickable3").click(function() {
+    $(".walrus-hidden2").fadeOut(2000);
+    $(".walrus-showing2").fadeOut(2000);
   });
 });
